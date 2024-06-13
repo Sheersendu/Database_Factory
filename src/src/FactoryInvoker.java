3b6a5037d9a3a2e51f12d0961617cbce31ca3bba
Sheersendu <shibrata15@gmail.com>
2024-06-14 00:54:42 +0530

    Mongo configuration + connection successfully established

--- src/src/FactoryInvoker.java
+++ src/src/FactoryInvoker.java
@@ -1,8 +1,31 @@
+import mongo_DB.MongoConfiguration;
 import postgres_DB.PostgresConfiguration;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class FactoryInvoker {
 	public static void main(String[] args) {
 		PostgresConfiguration postgresConfiguration = new PostgresConfiguration();
 		System.out.println(postgresConfiguration.getConnection());
+		MongoConfiguration mongoConfiguration = new MongoConfiguration();
+		Connection conn = mongoConfiguration.getConnection();
+		// Create a statement
+		Statement stmt;
+		try {
+			stmt = conn.createStatement();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+		// Execute a query
+		String sql = "SELECT * FROM Test_Collection";
+		try {
+			boolean rs = stmt.execute(sql);
+			System.out.println(rs);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
--- src/src/mongo_DB/MongoConfiguration.java
+++ src/src/mongo_DB/MongoConfiguration.java
@@ -1,12 +1,36 @@
 package mongo_DB;
 
+import config.ConfigReader;
 import interfaces.IDatabaseConfiguration;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class MongoConfiguration implements IDatabaseConfiguration {
+	private final Properties properties;
+
+	public MongoConfiguration() {
+		this.properties = ConfigReader.getConfiguration();
+	}
+
 	@Override
 	public Connection getConnection() {
-		return null;
+		String connectionString = "jdbc:mongodb://" + properties.getProperty("mongodb.url") + ":" + properties.getProperty("mongodb.port") + "/admin";
+		System.out.println(connectionString);
+		try {
+			Class.forName("mongodb.jdbc.MongoDriver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		Connection mongodbConnection;
+		try
+		{
+			mongodbConnection = DriverManager.getConnection(connectionString);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return mongodbConnection;
 	}
 }
